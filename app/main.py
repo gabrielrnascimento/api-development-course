bdcc1db95263530a7b0cd1597035a73d5cd98e0d
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2022-07-17 20:03:02 -0300

    feat(main.py): finished section 5 - python + raw sql
    add postgresql database integration, remove dummy test database data

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
-from logging import raiseExceptions
+from multiprocessing import current_process
 from typing import Optional
+import click
 from fastapi import Body, FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time 
+
+
+click.clear() # fix color codes on terminal
 
 app = FastAPI()
 
@@ -11,7 +18,25 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
+    # rating: Optional[int] = None
+
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            host='localhost', 
+            database='api-development-course', 
+            user='postgres', 
+            password='password123',
+            cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successfull!")
+        break
+    except Exception as error:
+        print("Connection to database failed")
+        print("Error: ", error)
+        time.sleep(2)
+
 
 # dummy database for testing CRUD operations
 my_posts = [
@@ -39,20 +64,40 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute(""" 
+        SELECT * 
+        FROM posts 
+        """)
+    posts = cursor.fetchall()
+    return {"data": posts}
 
-# ! everytime we create something we should return a 201 status code
+# everytime we create something we should return a 201 status code
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict["id"] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict} 
+    cursor.execute("""
+        INSERT INTO posts 
+            (title, content, published) 
+        VALUES
+            (%s, %s, %s)
+        RETURNING *
+        """, (post.title, post.content, post.published)
+        )
+    new_post = cursor.fetchone()
+
+    conn.commit()
+
+    return {"data": new_post} 
 
 # in order to get a specific post we should pass a path parameter
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response):
-    post = find_post(id)
+    cursor.execute("""
+        SELECT *
+        FROM posts
+        WHERE id = %s
+        """, (str(id))
+        )
+    post = cursor.fetchone()
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, 
@@ -61,25 +106,38 @@ def get_post(id: int, response: Response):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    # deleting post
-    # find the index in the array that has the required ID
-    #  my_posts.pop(index)
-    index = find_index_post(id)
-    if index == None:
+
+    cursor.execute("""
+    DELETE FROM posts
+    WHERE id = %s
+    RETURNING *
+    """, (str(id))
+    )
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
+    if deleted_post == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, 
             detail=f"post with id: {id} does not exist")
-    my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
-    if index == None:
+
+    cursor.execute("""
+    UPDATE posts SET 
+        title = %s, 
+        content = %s, 
+        published = %s
+    WHERE id = %s
+    RETURNING *
+    """, (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+
+    if updated_post == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, 
             detail=f"post with id: {id} does not exist")
-    post_dict = post.dict()
-    post_dict["id"] = id
-    my_posts[index] = post_dict
-    return {"data": post_dict}
+    return {"data": updated_post}
