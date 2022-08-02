7082c056300d5f5f876f03ac58a767bd8cc20d24
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2022-08-02 08:02:47 -0300

    feat(main.py-requirements.txt-.gitignore): finished section 12 - pre deployment checklist
    solved CORS issue and created requirements.txt file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import click
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
@@ -11,6 +13,16 @@ click.clear() # fix color codes on terminal
 # models.Base.metadata.create_all(bind=engine) # ! no more need for this command because we implemented Alembic
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ! routing magic !!!!
 app.include_router(post.router)
